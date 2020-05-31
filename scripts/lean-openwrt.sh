2a0adebd04750d027e8802d029e7ae52eec303d2
SuLingGG <1215009714@qq.com>
2020-05-31 21:38:07 +0800

    Modfiy Lean's OpenWrt scripts to fit upstream updates

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -45,7 +45,7 @@ cp luci-app-diskman/Parted.Makefile parted/Makefile
 git clone --depth=1 https://github.com/KFERMercer/luci-app-dockerman
 mkdir luci-lib-docker
 curl -s -o ./luci-lib-docker/Makefile https://raw.githubusercontent.com/lisaac/luci-lib-docker/master/Makefile
-rm -rf ../lean/luci-app-dockerman
+rm -rf ../lean/luci-app-docker
 
 # Add luci-app-gowebdav
 git clone --depth=1 https://github.com/project-openwrt/openwrt-gowebdav
