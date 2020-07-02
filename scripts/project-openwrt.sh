6ed2a29007c5aeda05bf8336d901577fce6675e3
SuLingGG <1215009714@qq.com>
2020-07-02 16:34:51 +0800

    Use Lean's baidupcs-web

--- scripts/project-openwrt.sh
+++ scripts/project-openwrt.sh
@@ -20,6 +20,12 @@ git clone --depth=1 https://github.com/jerrykuku/node-request
 git clone --depth=1 https://github.com/jerrykuku/luci-app-jd-dailybonus
 popd
 
+# Use Lean's baidupcs-web
+pushd package/lean
+rm -rf baidupcs-web
+svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/baidupcs-web
+popd
+
 # Add po2lmo
 git clone https://github.com/openwrt-dev/po2lmo.git
 pushd po2lmo
