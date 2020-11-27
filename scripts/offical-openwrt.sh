8b4361ac4d95f6499d7cd95d2113b0f409c0f4ab
SuLingGG <1215009714@qq.com>
2020-11-27 19:04:50 +0800

    Revert docker-ce to fe48902

--- scripts/offical-openwrt.sh
+++ scripts/offical-openwrt.sh
@@ -109,6 +109,9 @@ sed -i '/http/d' zzz-default-settings
 sed -i '/exit/i\chmod +x /bin/ipv6-helper' zzz-default-settings
 popd
 
+# Revert docker-ce to fe48902
+git checkout fe48902bf05131a1cbb86fbf8c01376955f86c12 feeds/packages/utils/docker-ce/
+
 # Mod ipv6-helper.sh
 sed -i '/filter_aaaa/d;/commit dhcp/d' ../scripts/ipv6-helper.sh
 
