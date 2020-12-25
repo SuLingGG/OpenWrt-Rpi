6fb990547080ed954903c823f5a058c98b8ded0e
SuLingGG <1215009714@QQ.COM>
2020-12-25 23:05:53 +0800

    Update project-openwrt.sh

--- scripts/project-openwrt.sh
+++ scripts/project-openwrt.sh
@@ -8,12 +8,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Use fixed autocore for raspberrypi
-pushd package/lean
-rm -rf autocore
-git clone --depth=1 https://github.com/SuLingGG/autocore -b lean
-popd
-
 mkdir package/community
 pushd package/community
 
