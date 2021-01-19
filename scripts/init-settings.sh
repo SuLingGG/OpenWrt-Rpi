40d8bc507b8377cb2c54cf48b9c617e717b27cb5
SuLingGG <1215009714@qq.com>
2021-01-19 15:40:22 +0800

    Disable autostart by default for nft-qos

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -9,8 +9,10 @@
 # Set default theme to luci-theme-argon
 uci set luci.main.mediaurlbase='/luci-static/argon'
 
-# Disable luci-udptools autostart
-rm -f /etc/rc.d/S98udptools || true
+# Disable autostart by default for some packages
+cd /etc/rc.d
+rm -f S98udptools || true
+rm -f S99nft-qos || true
 
 # Try to execute init.sh (if exists)
 
