7c910eed2ceb6ef3d270f7d129517bc5549f6189
SuLingGG <1215009714@qq.com>
2021-01-26 19:18:22 +0800

    3.5mm headphone jack support optimization

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -13,16 +13,4 @@ uci set luci.main.mediaurlbase='/luci-static/argon'
 cd /etc/rc.d
 rm -f S98udptools || true
 
-# Try to execute shell scripts at /boot/initconfig (if exists)
-if [ ! -d "/boot/initconfig" ]; then
-cp -r /boot/initconfig /tmp
-chmod +x /tmp/initconfig/*.sh
-bash /tmp/initconfig/*.sh
-fi
-
-# Add support for 3.5mm headphone jack for Raspberry Pi
-if grep -q "bcm27xx" "/etc/openwrt_release"; then
-    echo "dtparam=i2c_arm=on,audio=on" >> /boot/config.txt
-fi
-
 exit 0
