9baca93b59abbd708dc649c7ab98850161546cea
SuLingGG <1215009714@qq.com>
2021-01-31 17:52:41 +0800

    Remove useless files & Add full language support
    Add missing adguardhome package
    
    Switch luci-app-adguardhome to lienol's source

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -45,8 +45,7 @@ git clone --depth=1 -b master https://github.com/vernesong/OpenClash
 git clone --depth=1 https://github.com/rufengsuixing/luci-app-onliner
 
 # Add luci-app-adguardhome
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-app-adguardhome
-svn co https://github.com/project-openwrt/openwrt/trunk/package/ntlf9t/AdGuardHome
+svn co https://github.com/Lienol/openwrt/trunk/package/diy/luci-app-adguardhome
 
 # Add luci-app-diskman
 git clone --depth=1 https://github.com/SuLingGG/luci-app-diskman
