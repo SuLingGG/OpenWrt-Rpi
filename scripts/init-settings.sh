279fe9ef18ca80c0d0483225cc6aec6817431e35
Helmi Amirudin <helmilaw@gmail.com>
2021-04-06 06:21:36 +0700

    Update init-settings.sh
    remove custom shortcut

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -25,37 +25,6 @@ sed -i "s/lang 'zh_cn'/lang 'auto'/g" /etc/config/luci
 uci set luci.main.lang='en'
 sed -i "s/lang 'zh_cn'/lang 'en'/g" /etc/config/luci
 
-# Add shutdown, poweroff, reboot commands
-cat >> /etc/config/luci <<EOF
-
-
-config command
-	option name 'Shutdown'
-	option command 'halt'
-
-config command
-	option name 'Power Off'
-	option command 'poweroff'
-
-config command
-	option name 'Reboot'
-	option command 'reboot'
-
-config command
-	option name 'ShadowsocksR Restart'
-	option command '/etc/init.d/shadowsocksr restart'
-
-config command
-	option name 'ShadowsocksR Stop'
-	option command '/etc/init.d/shadowsocksr stop'
-
-config command
-	option name 'Restart Load Balance'
-	option command 'mwan3 restart'
-
-
-EOF
-
 #uci commit
 uci commit
 
