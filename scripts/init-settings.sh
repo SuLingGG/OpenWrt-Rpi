34f11c1a30aeba8842620d351113fe0b617f5178
SuLingGG <1215009714@QQ.COM>
2021-06-05 17:54:36 +0800

    Add support for USB_LAN78XX Pi Compute Module 4 carrier boards

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -13,4 +13,10 @@ uci set luci.main.mediaurlbase='/luci-static/argon'
 cd /etc/rc.d
 rm -f S98udptools || true
 
+# Add support for USB_LAN78XX Pi Compute Module 4 carrier boards
+cd /lib/firmware/brcm
+if [ `ls -l|grep '4-compute-module.txt'|wc -l` -eq 0 ]; then
+    cp brcmfmac43455-sdio.raspberrypi,4-model-b.txt brcmfmac43455-sdio.raspberrypi,4-compute-module.txt
+fi
+
 exit 0
