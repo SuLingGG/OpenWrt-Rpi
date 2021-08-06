41e80323e22d19e159b97c08cb92c31965d2f35b
Helmi Amirudin <helmilaw@gmail.com>
2021-08-06 22:42:08 +0700

    remove unused patches for language changer

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -9,22 +9,6 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Replace the default languange from zh_cn to en/auto before build
-if [ -f package/lean/default-settings/Makefile ];then
-	echo "  helmilog : default-settings/Makefile file available..."
-	sed -i 's/LUCI_LANG_zh-cn/LUCI_LANG_en/g' package/lean/default-settings/Makefile
-	echo "  helmilog : zh_cn under default-settings/Makefile lang has changed to en..."
-else
-	echo "  helmilog : default-settings/Makefile file is not available. Skipping..."
-fi
-if [ -f package/lean/default-settings/files/zzz-default-settings ];then
-	echo "  helmilog : zzz-default-settings file available..."
-	sed -i 's/zh_cn/en/g' package/lean/default-settings/files/zzz-default-settings
-	echo "  helmilog : zh_cn under zzz-default-settings lang has changed to en..."
-else
-	echo "  helmilog : zzz-default-settings file is not available. Skipping..."
-fi
-
 # Add luci-app-ssr-plus
 pushd package/lean
 git clone --depth=1 https://github.com/fw876/helloworld
