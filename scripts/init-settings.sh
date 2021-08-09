3efc4c2ff9650cacfa58f2afba8d712a6c31c764
hx210 <76199340+hx210@users.noreply.github.com>
2021-08-09 12:48:17 +0800

    n

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -11,10 +11,10 @@ uci set luci.main.mediaurlbase='/luci-static/argon'
 
 uci set network.lan.ipaddr='192.168.22.1'                                    # IPv4 地址(openwrt后台地址)
 uci set network.lan.netmask='255.255.255.0'                                 # IPv4 子网掩码
-uci set network.lan.gateway='192.168.22.1'                                   # IPv4 网关
-uci set network.lan.broadcast='192.168.22.255'                               # IPv4 广播
+#uci set network.lan.gateway='192.168.22.1'                                   # IPv4 网关
+#uci set network.lan.broadcast='192.168.22.255'                               # IPv4 广播
 uci set network.lan.dns='192.168.22.1'                         # DNS(多个DNS要用空格分开)
-uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
+#uci set network.lan.delegate='0'                                            # 去掉LAN口使用内置的 IPv6 管理
 uci commit network                                                          # 不要删除跟注释,除非上面全部删除或注释掉了
 #uci set dhcp.lan.ignore='1'                                                 # 关闭DHCP功能
 #uci commit dhcp                                                             # 跟‘关闭DHCP功能’联动,同时启用或者删除跟注释
