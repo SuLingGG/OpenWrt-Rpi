43cc0dd82a7c25f55b1eb65c654d905a5b50ea12
Helmi Amirudin <helmilaw@gmail.com>
2021-08-13 19:35:10 +0700

    change badvpn & corkscrew source

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -72,7 +72,7 @@ git clone --depth=1 https://github.com/sirpdboy/luci-theme-opentopd.git
 git clone --depth=1 https://github.com/rickydee/luci-proto-modemmanager.git #For OpenWrt 19.xx and above only. 
 
 # Add openwrt badvpn-tun2socks
-git clone --depth=1 https://github.com/friyadhibiermann/openwrt_badvpn-tun2socks
+git clone --depth=1 https://github.com/helmiau/badvpn_corkscrew
 
 # Add modeminfo
 git clone --depth=1 https://github.com/koshev-msk/luci-app-modeminfo
