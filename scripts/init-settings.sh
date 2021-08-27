a4e3cbd9b4a63eb5b481e9fcf19ede51e340538c
SuLingGG <1215009714@QQ.COM>
2021-08-27 17:30:00 +0800

    OPkg: Disable opkg signature check

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -9,6 +9,9 @@
 # Set default theme to luci-theme-argon
 uci set luci.main.mediaurlbase='/luci-static/argon'
 
+# Disable opkg signature check
+sed -i 's/option check_signature/# option check_signature/g' /etc/opkg.conf
+
 # Disable autostart by default for some packages
 cd /etc/rc.d
 rm -f S98udptools || true
