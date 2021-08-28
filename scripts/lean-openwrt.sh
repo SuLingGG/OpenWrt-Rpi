182e9503f64456c39a3e78ffec5c0a0226f1fcc7
SuLingGG <1215009714@QQ.COM>
2021-08-28 18:29:13 +0800

    Opkg: Correct opkg mirror config

--- scripts/lean-openwrt.sh
+++ scripts/lean-openwrt.sh
@@ -84,6 +84,7 @@ popd
 # Mod zzz-default-settings
 pushd package/lean/default-settings/files
 sed -i '/http/d' zzz-default-settings
+sed -i '/18.06/d' zzz-default-settings
 export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
 sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d"))/g" zzz-default-settings
 popd
