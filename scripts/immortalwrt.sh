951302df394644fb7d95e7fcd57d633b93fef7c5
SuLingGG <1215009714@qq.com>
2021-10-17 13:01:18 +0800

    Scripts: Fix toolchain compile

--- scripts/immortalwrt.sh
+++ scripts/immortalwrt.sh
@@ -63,10 +63,10 @@ sed -i 's/ImmortalWrt/OpenWrt/g' config_generate
 popd
 
 # Fix SDK
-sed -i '/$(SDK_BUILD_DIR)\/$(STAGING_SUBDIR_HOST)\/usr\/bin/d;/LICENSE/d' target/sdk/Makefile
+sed -i '/$(SDK_BUILD_DIR)\/$(STAGING_SUBDIR_HOST)\/usr\/bin/d' target/sdk/Makefile
 
-# Remove some default packages
-sed -i 's/luci-app-ddns//g;s/luci-app-upnp//g;s/luci-app-adbyby-plus//g;s/luci-app-vsftpd//g;s/luci-app-ssr-plus//g;s/luci-app-unblockmusic//g;s/luci-app-vlmcsd//g;s/luci-app-wol//g;s/luci-app-nlbwmon//g;s/luci-app-accesscontrol//g' include/target.mk
+# Fix Toolchain
+sed -i 's/LICENSE/LICENSES/g' target/toolchain/Makefile
 
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
