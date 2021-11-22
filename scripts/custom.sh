3f329e27170bc9671553ce135735169c637afced
SuLingGG <1215009714@QQ.COM>
2021-11-22 12:54:00 +0800

    Scripts: Remove outdate packages

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -12,33 +12,18 @@
 pushd package/emortal/default-settings/files
 sed -i '/http/d' zzz-default-settings
 sed -i '/openwrt_luci/d' zzz-default-settings
-export orig_version=$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')
-export date_version=$(date -d "$(rdate -n -4 -p pool.ntp.org)" +'%Y-%m-%d')
-sed -i "s/${orig_version}/${orig_version} (${date_version})/g" zzz-default-settings
 popd
+
+# Add date version
+export DATE_VERSION=$(date -d "$(rdate -n -4 -p pool.ntp.org)" +'%Y-%m-%d')
+sed -i "s/%C/%C (${DATE_VERSION})/g" package/base-files/files/etc/openwrt_release
 
 # Clone community packages to package/community
 mkdir package/community
 pushd package/community
 
-# Add luci-app-bypass
-git clone --depth=1 https://github.com/garypang13/luci-app-bypass
-git clone --depth=1 https://github.com/garypang13/smartdns-le
-
-# Add luci-app-godproxy
-git clone --depth=1 https://github.com/project-lede/luci-app-godproxy
-
-# Add luci-app-modeminfo
-git clone --depth=1 https://github.com/koshev-msk/luci-app-modeminfo
-
-# Add luci-app-tcpdump
-git clone --depth=1 https://github.com/KFERMercer/luci-app-tcpdump
-
 # Add luci-app-oaf
 git clone --depth=1 https://github.com/destan19/OpenAppFilter -b oaf-3.0.1
-
-# Add luci-theme-argon_armygreen
-git clone --depth=1 https://github.com/XXKDB/luci-theme-argon_armygreen
 popd
 
 # Fix mt76 wireless driver
