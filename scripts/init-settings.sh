7ba5e667f6c321a27ab0307e0a0bf21113a7d48e
SuLingGG <1215009714@QQ.COM>
2021-11-22 12:54:19 +0800

    Scripts: Disable IPV6 ula prefix by default

--- scripts/init-settings.sh
+++ scripts/init-settings.sh
@@ -9,6 +9,9 @@
 # Set default theme to luci-theme-argon
 uci set luci.main.mediaurlbase='/luci-static/argon'
 
+# Disable IPV6 ula prefix
+sed -i 's/^[^#].*option ula/#&/' /etc/config/network
+
 # Check file system during boot
 uci set fstab.@global[0].check_fs=1
 uci commit
