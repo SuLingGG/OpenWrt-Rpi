d7f8bed1565fca6e8c26132a3a974aef739ad640
SuLingGG <1215009714@QQ.COM>
2021-12-07 16:36:00 +0800

    Scripts: Remove outdate fix for sdk & toolchain

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -36,11 +36,5 @@ pushd package/base-files/files/bin
 sed -i 's/ImmortalWrt/OpenWrt/g' config_generate
 popd
 
-# Fix SDK
-sed -i '/$(SDK_BUILD_DIR)\/$(STAGING_SUBDIR_HOST)\/usr\/bin/d' target/sdk/Makefile
-
-# Fix Toolchain
-sed -i 's/LICENSE/LICENSES/g' target/toolchain/Makefile
-
 # Change default shell to zsh
 sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
