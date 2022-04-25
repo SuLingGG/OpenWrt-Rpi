31c574d043d65328d6c8d7fb9cab388941336445
SuLingGG <1215009714@QQ.COM>
2022-04-25 22:43:17 +0800

    Scripts: Remove outdate lines

--- scripts/custom.sh
+++ scripts/custom.sh
@@ -8,23 +8,9 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-# Mod zzz-default-settings
-pushd package/emortal/default-settings/files
-sed -i '/http/d' zzz-default-settings
-sed -i '/openwrt_luci/d' zzz-default-settings
-popd
-
 # Add date version
 export DATE_VERSION=$(date -d "$(rdate -n -4 -p pool.ntp.org)" +'%Y-%m-%d')
 sed -i "s/%C/%C (${DATE_VERSION})/g" package/base-files/files/etc/openwrt_release
-
-# Clone community packages to package/community
-mkdir package/community
-pushd package/community
-
-# Add luci-app-oaf
-git clone --depth=1 https://github.com/destan19/OpenAppFilter -b oaf-3.0.1
-popd
 
 # Fix mt76 wireless driver
 pushd package/kernel/mt76
